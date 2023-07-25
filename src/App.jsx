6c074a9fee9c08255a56cc7dbc30d1eba1f8fdeb
Harold <cvuyante24@gmail.com>
2023-07-25 11:53:16 +0800

    third commit: add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-// https://pokeapi.co/api/v2/pokemon/ditto
-
 import useFetch from './component/useFetch';
+import './assets/index.css';
 
 function App() {
 	const { data, loading, error } = useFetch(
-		'https://pokeapi.co/api/v2/pokemon/ditto'
+		'https://fakestoreapi.com/products/1'
 	);
 
 	if (loading) return <h1>LOADING .....</h1>;
@@ -12,10 +11,23 @@ function App() {
 
 	return (
 		<>
-			<h1>{data?.name}</h1>
-			{data?.abilities.map((item, index) => (
+			<div className='h-96 w-full m-4'>
+				<img
+					src={data?.image}
+					alt={data?.title}
+					className='h-52 w-52 object-contain'
+				/>
+				<h1>{data?.title}</h1>
+				<h2>{data?.price}</h2>
+				<div className='flex flex-row'>
+					<div className='mr-4'>{data?.rating?.rate}</div>
+					<div>{data?.rating?.count}</div>
+				</div>
+			</div>
+			;
+			{/* {data?.abilities.map((item, index) => (
 				<p key={index}>{item.ability.name}</p>
-			))}
+			))} */}
 		</>
 	);
 }
